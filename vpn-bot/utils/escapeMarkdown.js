21d1a746743ab7874a5483a3110c5b05085ef624
Daniil Mitrofanov <dd.mitrofanov@gmail.com>
2026-02-16 21:39:34 +0300

    fix regex

--- vpn-bot/utils/escapeMarkdown.js
+++ vpn-bot/utils/escapeMarkdown.js
@@ -1,10 +1,12 @@
 /**
- * Экранирование символов для Telegram Markdown: _ * ` [
+ * Экранирование символов для Telegram Markdown: _ * ` [ ]
  * Использовать для любого пользовательского или внешнего текста перед отправкой с parse_mode: 'Markdown'
  */
+const MARKDOWN_SPECIAL = /[\]_*`[]/g;
+
 function escapeMarkdown(s) {
   if (s == null || typeof s !== 'string') return '';
-  return s.replace(/([_*`[\])/g, '\\$1');
+  return s.replace(MARKDOWN_SPECIAL, '\\$&');
 }
 
 module.exports = { escapeMarkdown };
